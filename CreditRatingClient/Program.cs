3aa227e3542081700595cb28a1857c2122a2eb47
Andrea Chiarelli <andrea.chiarelli@auth0.com>
2020-02-06 17:13:45 +0100

    Added secured communication

--- CreditRatingClient/Program.cs
+++ CreditRatingClient/Program.cs
@@ -3,30 +3,117 @@ using System.Threading.Tasks;
 using CreditRatingService;
 using Grpc.Net.Client;
 using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using Grpc.Core;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
 
 namespace CreditRatingClient
 {
-    class Program
+  class Program
+  {
+    private static readonly HttpClient httpClient = new HttpClient();
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            var serverAddress = "https://localhost:5001";
+      var serverAddress = "https://localhost:5001";
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
-                // The following statement allows you to call insecure services. To be used only in development environments.
-                AppContext.SetSwitch(
-                    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-                serverAddress = "http://localhost:5000";
-            }
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+      {
+        // The following statement allows you to call insecure services. To be used only in development environments.
+        AppContext.SetSwitch(
+            "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+        serverAddress = "http://localhost:5000";
+      }
 
-            var channel = GrpcChannel.ForAddress(serverAddress);
-            var client =  new CreditRatingCheck.CreditRatingCheckClient(channel);
-            var creditRequest = new CreditRequest { CustomerId = "id0201", Credit = 7000};
-            var reply = await client.CheckCreditRequestAsync(creditRequest);
+      var channel = GrpcChannel.ForAddress(serverAddress);
+      var client = new CreditRatingCheck.CreditRatingCheckClient(channel);
+      var creditRequest = new CreditRequest { CustomerId = "id0201", Credit = 7000 };
 
-            Console.WriteLine($"Credit for customer {creditRequest.CustomerId} {(reply.IsAccepted ? "approved" : "rejected")}!");
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
+      var accessToken = await GetAccessToken();
+      var headers = new Metadata();
+      headers.Add("Authorization", $"Bearer {accessToken}");
+
+      var reply = await client.CheckCreditRequestAsync(creditRequest, headers);
+
+      Console.WriteLine($"Credit for customer {creditRequest.CustomerId} {(reply.IsAccepted ? "approved" : "rejected")}!");
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
     }
+
+    // Using an authorized channel doesn't work on MacOS
+    //static async Task Main(string[] args)
+    //{
+    //    //// The port number(5001) must match the port of the gRPC server.
+    //    var channel = await CreateAuthorizedChannel("https://localhost:5001");
+    //    var client = new CreditRatingCheck.CreditRatingCheckClient(channel);
+    //    var creditRequest = new CreditRequest { CustomerId = "id0201", Credit = 7000 };
+    //    var reply = await client.CheckCreditRequestAsync(creditRequest);
+
+    //    Console.WriteLine($"Credit for customer {creditRequest.CustomerId} {(reply.IsAccepted ? "approved" : "rejected")}!");
+    //    Console.WriteLine("Press any key to exit...");
+    //    Console.ReadKey();
+    //}
+
+    static IConfiguration GetAppSettings()
+    {
+      var builder = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json");
+
+      return builder.Build();
+    }
+
+    static async Task<string> GetAccessToken()
+    {
+      var appAuth0Settings = GetAppSettings().GetSection("Auth0");
+
+            var requestContent = JsonSerializer.Serialize(new {
+                client_id = appAuth0Settings["ClientId"],
+                client_secret = appAuth0Settings["ClientSecret"],
+                audience = appAuth0Settings["Audience"],
+                grant_type = "client_credentials"
+            });
+      //var requestContent = $"{{" +
+      //        $"\"client_id\":\"{appAuth0Settings["ClientId"]}\"," +
+      //        $"\"client_secret\":\"{appAuth0Settings["ClientSecret"]}\"," +
+      //        $"\"audience\":\"{appAuth0Settings["Audience"]}\"," +
+      //        $"\"grant_type\":\"client_credentials\"" +
+      //    $"}}";
+      var response = await httpClient.PostAsync($"https://{appAuth0Settings["Domain"]}/oauth/token",
+              new StringContent(requestContent,
+              UnicodeEncoding.UTF8,
+              "application/json"));
+      var responseString = response.Content.ReadAsStringAsync().Result;
+
+      var responseObj = JsonSerializer.Deserialize<Dictionary<string, Object>>(responseString);
+
+      return responseObj["access_token"].ToString();
+    }
+
+    private async static Task<GrpcChannel> CreateAuthorizedChannel(string address)
+    {
+      var accessToken = await GetAccessToken();
+
+      var credentials = CallCredentials.FromInterceptor((context, metadata) =>
+      {
+        if (!string.IsNullOrEmpty(accessToken))
+        {
+          metadata.Add("Authorization", $"Bearer {accessToken}");
+        }
+        return Task.CompletedTask;
+      });
+
+      // SslCredentials is used here because this channel is using TLS.
+      // CallCredentials can't be used with ChannelCredentials.Insecure on non-TLS channels.
+      var channel = GrpcChannel.ForAddress(address, new GrpcChannelOptions
+      {
+        Credentials = ChannelCredentials.Create(new SslCredentials(), credentials)
+      });
+      return channel;
+    }
+  }
 }
--- CreditRatingService/Startup.cs
+++ CreditRatingService/Startup.cs
@@ -3,37 +3,57 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace CreditRatingService
 {
-    public class Startup
+  public class Startup
+  {
+    public IConfiguration Configuration { get; }
+    public Startup(IConfiguration configuration)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddGrpc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<CreditRatingCheckService>();
-
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                });
-            });
-        }
+      Configuration = configuration;
     }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAuthentication(options =>
+      {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      }).AddJwtBearer(options =>
+      {
+        options.Authority = $"https://{Configuration["Auth0:Domain"]}/";
+        options.Audience = Configuration["Auth0:Audience"];
+      });
+      services.AddAuthorization();
+      services.AddGrpc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapGrpcService<CreditRatingCheckService>();
+
+        endpoints.MapGet("/", async context =>
+              {
+                await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+              });
+      });
+    }
+  }
 }
